09203d109b475c5203737f2029fdfd523ab887d8
sarah dagamseh <dagamsehsarah@gmail.com>
2021-03-07 15:12:18 +0200

    starter-code

---
+++ starter-code.js
@@ -0,0 +1,31 @@
+# HOW HTML, CSS, AND JAVASCRIPT FIT TOGETHER
+ 
+### <HTML>
+
+#### Content Layer.html files
+##### This is where the content of the page lives. The HTML gives the page structure and add ssemantics.
+
+### <CSS>
+
+#### PRESENTATION LAYER.css files
+
+The CSS enhances the HTML page with rules that state how the HTML content is presented (backgrounds, borders, box dimensions, colors, fonts, etc.).
+
+### <JAVASCRIPT>
+
+#### BEHAVIOR LAYER.js files
+
+##### This is where we can change how the page behaves, adding interact ivity. We will aim to keep as much of our JavaScript as possible in separate files.
+
+
+#### HTML ONLY
+##### Starting with the HTML layer allows you to focus on the most important thing about your site: its content. Being plain HTML, this layer should work on all kinds of devices, be accessible to all users, and load quite quickly on
+ slow connections.
+
+ #### HTML+CSS
+ ##### Adding the CSS rules in a separate file keeps rules regarding how the page looks away from the content itself. You can use the same style sheet with all of your site, making your sites faster to load and easier to maintain. Or you can use different style sheets with the same content to create different views of the same data.
+
+ #### HTML+CSS+JAVASCRIPT
+
+ ##### The JavaScript is added last and enhances the usability of the page or the experience of interacting with the si te. Keeping it separate means that the page still works if the user cannot load or run the JavaScript. You can also reuse the code on severa l pages (making the site faster to load and easier to maintain).
+ 
